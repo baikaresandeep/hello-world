98cb693e8a937e41b254ab227e99c3475c3cae2d
baikaresandeep <baikare.sandeep007@gmail.com>
2017-08-29 19:03:23 -0400

    Custom walker function has been added

---
+++ Custom_taxonomy_list.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * [custom_taxonomy_walker description]
+ * @param  string  $taxonomy  name of taxonomy
+ * @param  integer $parent    parent term_id of term
+ * @return string             generate the list of taxonomy
+ */
+    function custom_taxonomy_walker( $taxonomy, $parent = 0 ){
+        $terms = get_terms($taxonomy, array('parent' => $parent, 'hide_empty' => false, 'orderby'    => 'name'));
+        //If there are terms, start displaying
+        if(count($terms) > 0)
+        {
+            //Displaying as a list
+            $out = "<ul>";
+            //Cycle though the terms
+            foreach ($terms as $term)
+            {
+                //Secret sauce.  Function calls itself to display child elements, if any
+                $out .="<li class='term term-" . $term->slug ."'>" . 
+                            "<a href='javascript:void(0)' data-term_id='" . $term->term_id . "' data-tax='". $taxonomy ."'> " . 
+                            $term->name .
+                            "</a>".
+                            custom_taxonomy_walker( $taxonomy, $term->term_id ) . 
+                        "</li>"; 
+            }
+            $out .= "</ul>";    
+            return $out;
+        }
+        return;
+    }
+
+// Usage 
+echo  custom_taxonomy_walker( $taxonomy, $parent = 0 );
